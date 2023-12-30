17e2a9a1e77b46ae6e2cd564b694ac49b1ff37c4
mdrabbi97324 <mdrabbi97324@gmail.com>
2023-12-30 20:59:28 +0600

    listing dynamic id

--- public/index.php
+++ public/index.php
@@ -1,8 +1,19 @@
 <?php
 
+require __DIR__ . "/../vendor/autoload.php";
 require "../helpers.php";
-require basePath("Router.php");
-require basePath("Database.php");
+
+use Framework\Router;
+
+
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+
+//     if(file_exists($path)){
+//         require $path;
+//     }
+// });
+
 
 
 //Initiate the router
@@ -14,12 +25,10 @@ $routes = require basePath("routes.php");
 
 // Get current uri and HTTP method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+
+
 
 // Route the request
-$router->route($uri, $method);
-
-
-
+$router->route($uri);
 
 
